3030e0c559edf1a70a483795583492e572ce4eca
tom-ramesh <tomramesh92@gmail.com>
2023-04-02 11:48:55 +0530

    scroll reveal wrapped in DOM content loaded

--- src/app.ts
+++ src/app.ts
@@ -47,17 +47,19 @@ window.onscroll = () => {
 };
 
 //scroll reveal
-ScrollReveal({
-  reset: true,
-  distance: "80px",
-  duration: 2000,
-  delay: 200,
-});
+document.addEventListener("DOMContentLoaded", () => {
+  ScrollReveal({
+    reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+  });
 
-ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
-ScrollReveal().reveal(
-  ".home-img, .services-container, .portfolio-box, .contact form",
-  { origin: "bottom" }
-);
-ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
-ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+  ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+  ScrollReveal().reveal(
+    ".home-img, .services-container, .portfolio-box, .contact form",
+    { origin: "bottom" }
+  );
+  ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+  ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+});
